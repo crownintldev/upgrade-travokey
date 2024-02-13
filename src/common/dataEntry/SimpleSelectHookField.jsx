9e6c65c8b80fc54081bcbfb2807d88613cb24f06
crownintldev <crownintltechnology@gmail.com>
2024-02-13 17:59:24 +0500

    salik-committed $(date +'%d-%m-%y %I:%M %p')

--- src/common/dataEntry/SimpleSelectHookField.jsx
+++ src/common/dataEntry/SimpleSelectHookField.jsx
@@ -48,6 +48,7 @@
 
 // export default SimpleSelectHookField
 
+// updated component
 import React from 'react'
 import { Controller } from 'react-hook-form'
 import { CaretSortIcon, CheckIcon } from '@radix-ui/react-icons'
